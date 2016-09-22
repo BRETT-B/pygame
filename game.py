e1f432ef5ac6e40a80fcdb0a981ef9fbfbf233a5
Brett D Burdick <burdickbrett@outlook.com>
2016-09-22 13:33:33 -0400

    Movement on hero fixed

--- game.py
+++ game.py
@@ -1,20 +1,21 @@
 
-import pygame
-from hero import Hero
+import pygame #duh
+from hero import Hero #bring in the hero class with all its methods and glory
 from settings import Settings
 import game_functions as gf
 
-
-# Set up the main core function
+#set up the main core function
 def run_game():
-	pygame.init()
-	game_settings = Settings()
-	screen = pygame.display.set_mode(game_settings.screen_size)
-	pygame.display.set_caption("Monster Attack")
-	hero = Hero(screen)
-	while 1:
-		gf.check_events(hero) #call gf (alias from game_functions) module and get check_events method
-		hero.update() #update the hero flags
-		gf.update_screen(game_settings, screen, hero) #call update screen method
+	pygame.init() # initialize all the pygame modules
+	game_settings = Settings() #create an instance of settings class
+	screen = pygame.display.set_mode(game_settings.screen_size) #set the screen size with set_mode
+	pygame.display.set_caption("Monster Attack") #set the message on the status bar
+	hero = Hero(screen) # set a variable equal to the class and pass it the screen
 
-run_game() #Start the game!
+	while 1: #run this loop forever...
+		gf.check_events(hero) #call gf (aliased from game_functions module) and get the check_events
+		hero.update() #update the hero flags
+		gf.update_screen(game_settings, screen, hero) # call the update_screen method
+				
+
+run_game() #start the game	
--- game_functions.py
+++ game_functions.py
@@ -1,28 +1,32 @@
-# We will put all main game functions here
+# we will put all main game functions here
 import sys
 import pygame
 
-
-
 def check_events(hero):
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT: 
-			sys.exit()
+	for event in pygame.event.get(): #run through all pygame events
+		if event.type == pygame.QUIT: #if the event is the quit event...
+			sys.exit() #quit
+		elif event.type == pygame.KEYDOWN: #the user pushed a key and it's down
+			if event.key == pygame.K_RIGHT: #the user pressed right	
+				hero.moving_right = True #set the flag
+			elif event.key == pygame.K_LEFT:
+				hero.moving_left = True #set the flag
+			elif event.key == pygame.K_UP: 	
+				hero.moving_up = True #set the flag	
+			elif event.key == pygame.K_DOWN: 	
+				hero.moving_down = True #set the flag		
+		elif event.type == pygame.KEYUP: #user let go of a key
+			if event.key == pygame.K_RIGHT: #specifically the right arrow
+				hero.moving_right = False
+			elif event.key == pygame.K_LEFT: 
+				hero.moving_left = False
+			elif event.key == pygame.K_UP: 
+				hero.moving_up = False	
+			elif event.key == pygame.K_DOWN: 
+				hero.moving_down = False				
 
-		# MOVE THE HERO
-		elif event.type == pygame.KEYDOWN: #the user pressed a key
-			if event.key == pygame.K_RIGHT:
-				# Move the hero to the right
-				hero.moving_right == True #set the flag
-			elif event.key == pygame.K_LEFT:
-				hero.moving_left == True #set the flag
-		# STOP THE HERO MOVEMENT
-		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_RIGHT:
-				hero.moving_right == False 
-			elif event.key == pygame.K_LEFT:
-				hero.moving_left == False
-def update_screen(game_settings, screen, hero):
-	screen.fill(settings.bg_color)
-	hero.draw_me() #call the draw method imported from Hero class
+def update_screen(settings, screen, hero):
+	screen.fill(settings.bg_color) #fill the background with our green
+	hero.draw_me() #call the draw method and put the hero on our screen
 	pygame.display.flip()
+
